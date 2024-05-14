9e175c3cbd87fb7cdd1189fe41cd2368c74c7b12
Josue Cota <xJaramis@gmail.com>
2024-05-14 00:56:19 -0700

    I hope this isnt the solution

--- portfolio/src/Components/Contacts.jsx
+++ portfolio/src/Components/Contacts.jsx
@@ -1,5 +1,9 @@
 import {motion} from "framer-motion"
 import style from "../pages/contacts.module.css"
+import gitImg from "../../public/images/github.svg"
+import emailImg from "../../public/images/email.svg"
+import linkedImg from "../../public/images/linkedin.svg"
+import resumeImg from "../../public/images/resume.svg"
 
 //Specifc animation each item does, fade in in this case
 const item = {
@@ -20,10 +24,10 @@ export default function Contacts() {
                 Hi! Thank you for checking out my Porfolio. If you would like to connect, reach out to me through any of the following!
             </p>
             <motion.ul className={style.list}>
-                <motion.li variants={item}><a href="https://github.com/JosueCota" target="_blank"><img src="./src/public/images/github.svg"/>Github</a></motion.li>
-                <motion.li variants={item}><a href="https://www.linkedin.com/in/josue-a-cota/" target="_blank"><img src="./src/public/images/linkedin.svg"/>LinkedIn</a></motion.li>
-                <motion.li variants={item}><a href="mailto:xjaramis@gmail.com" target="_blank"><img src="./src/public/images/email.svg"/>Email</a></motion.li>
-                <motion.li variants={item}><a href="https://docs.google.com/document/d/e/2PACX-1vRcNHTs2oZXLaLGfOXrOlxdriuuj97gs9n5U6rhv4U9RTzxayuiPXW5YMCjjXUsKHuHjkvzh_MwBFAg/pub" target="_blank"><img src="./src/public/images/resume.svg"/>Resume</a></motion.li>
+                <motion.li variants={item}><a href="https://github.com/JosueCota" target="_blank"><img src={gitImg}/>Github</a></motion.li>
+                <motion.li variants={item}><a href="https://www.linkedin.com/in/josue-a-cota/" target="_blank"><img src={linkedImg}/>LinkedIn</a></motion.li>
+                <motion.li variants={item}><a href="mailto:xjaramis@gmail.com" target="_blank"><img src={emailImg}/>Email</a></motion.li>
+                <motion.li variants={item}><a href="https://docs.google.com/document/d/e/2PACX-1vRcNHTs2oZXLaLGfOXrOlxdriuuj97gs9n5U6rhv4U9RTzxayuiPXW5YMCjjXUsKHuHjkvzh_MwBFAg/pub" target="_blank"><img src={resumeImg}/>Resume</a></motion.li>
             </motion.ul>
             </div>
     )
