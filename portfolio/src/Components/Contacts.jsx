62e42a889cccbd561c57c3837d5008320c1a2abf
Josue Cota <xJaramis@gmail.com>
2024-05-14 00:50:34 -0700

    Not sure if that worked tbh

--- portfolio/src/Components/Contacts.jsx
+++ portfolio/src/Components/Contacts.jsx
@@ -20,10 +20,10 @@ export default function Contacts() {
                 Hi! Thank you for checking out my Porfolio. If you would like to connect, reach out to me through any of the following!
             </p>
             <motion.ul className={style.list}>
-                <motion.li variants={item}><a href="https://github.com/JosueCota" target="_blank"><img src="./src/assets/images/icons/contacts/github.svg"/>Github</a></motion.li>
-                <motion.li variants={item}><a href="https://www.linkedin.com/in/josue-a-cota/" target="_blank"><img src="./src/assets/images/icons/contacts/linkedin.svg"/>LinkedIn</a></motion.li>
-                <motion.li variants={item}><a href="mailto:xjaramis@gmail.com" target="_blank"><img src="./src/assets/images/icons/contacts/email.svg"/>Email</a></motion.li>
-                <motion.li variants={item}><a href="https://docs.google.com/document/d/e/2PACX-1vRcNHTs2oZXLaLGfOXrOlxdriuuj97gs9n5U6rhv4U9RTzxayuiPXW5YMCjjXUsKHuHjkvzh_MwBFAg/pub" target="_blank"><img src="./src/assets/images/icons/contacts/resume.svg"/>Resume</a></motion.li>
+                <motion.li variants={item}><a href="https://github.com/JosueCota" target="_blank"><img src="./src/public/images/icons/contacts/github.svg"/>Github</a></motion.li>
+                <motion.li variants={item}><a href="https://www.linkedin.com/in/josue-a-cota/" target="_blank"><img src="./src/public/images/icons/contacts/linkedin.svg"/>LinkedIn</a></motion.li>
+                <motion.li variants={item}><a href="mailto:xjaramis@gmail.com" target="_blank"><img src="./src/public/images/icons/contacts/email.svg"/>Email</a></motion.li>
+                <motion.li variants={item}><a href="https://docs.google.com/document/d/e/2PACX-1vRcNHTs2oZXLaLGfOXrOlxdriuuj97gs9n5U6rhv4U9RTzxayuiPXW5YMCjjXUsKHuHjkvzh_MwBFAg/pub" target="_blank"><img src="./src/public/images/icons/contacts/resume.svg"/>Resume</a></motion.li>
             </motion.ul>
             </div>
     )
